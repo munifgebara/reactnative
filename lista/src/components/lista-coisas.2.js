33606e9bb9d8c5e69a08d7c6c677dce373942a8c
Munif Gebara Junior <munif@macnif.home>
2017-12-06 05:42:13 -0200

    ˜remove˜

--- lista/src/components/lista-coisas.2.js
+++ lista/src/components/lista-coisas.2.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import { setTimeout } from 'core-js/library/web/timers';
+import Button from './button';
 
 
 class ListaCoisas extends Component {
@@ -11,28 +12,58 @@ class ListaCoisas extends Component {
         this.carregaLista();
     }
 
+    comprar(coisa) {
+        
+        let l = this.state.lista.slice(0);
+        let i=l.indexOf(coisa);
+        l.splice(i,1);
+        //alert(coisa.produto);
+        this.setState({lista:l});
+    }
+
     renderCoisa() {
-        return this.state.lista.map(coisa => <Text key={coisa.produto}>{coisa.produto}</Text>);
+        const { imagemStyle, containerStyle } = estilos;
+        return this.state.lista.map(coisa =>
+            <View style={containerStyle} key={coisa.id}>
+                <Text>{coisa.produto}</Text>
+                <Image style={imagemStyle} source={{ uri: coisa.imagem }} />
+                <Button onPress={() => this.comprar(coisa)}>Remover</Button>
+            </View>
+        );
     }
 
     render() {
         return (
-            <View>
+            <ScrollView>
                 {this.renderCoisa()}
-            </View>
+            </ScrollView>
         );
     }
 
     carregaLista() {
         fetch('https://raw.githubusercontent.com/munifgebara/reactnative/master/lista/lista.json')
-            .then(response =>  response.json().then(data=>{
-                this.setState({lista:data});
+            .then(response => response.json().then(data => {
+                this.setState({ lista: data });
             }))
             .catch(error => {
-                this.setState({lista:[{produto:'Impossível carregar a lista'}]});
+                this.setState({ lista: [{ produto: 'Impossível carregar a lista' }] });
             });
 
     }
 }
+
+const estilos = {
+    imagemStyle: {
+        height: 300,
+
+        width: 300
+
+    },
+    containerStyle: {
+        alignItems: 'center',
+        flexDirection: 'column'
+    }
+};
+
 
 export default ListaCoisas;
