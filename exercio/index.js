cf1e0094ecd83618d5585a8c9bb2bb95530efde5
Munif Gebara Junior <munif@macnif.local>
2017-12-11 22:21:08 -0200

    fim segunda

--- exercio/index.js
+++ exercio/index.js
@@ -1,4 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Titulo from './src/componentes/titulo';
+import ListaCoisas from './src/componentes/lista-coisas';
+
+const App = () => {
+    return (
+        <View>
+            <Titulo texto={'Lista de Compras'} />
+            <ListaCoisas />
+        </View>
+    );
+};
+
+// http://dontpad.com/munif/react
 
 AppRegistry.registerComponent('exercio', () => App);
