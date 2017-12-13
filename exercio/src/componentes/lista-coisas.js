1d93f80a30aa76c93ed16eb676800120d6b7e459
Munif Gebara Junior <munif@macnif.local>
2017-12-13 21:48:20 -0200

    Adicionado botao

--- exercio/src/componentes/lista-coisas.js
+++ exercio/src/componentes/lista-coisas.js
@@ -1,43 +1,75 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
+import Button from './button';
+
 
 class ListaCoisas extends Component {
 
-    state = { lista: [] };
+    state = { lista: [{ id: 0, produto: 'Carregando...' }] };
 
-    componentWillMount() { 
+    urlDaApi = 'https://raw.githubusercontent.com/munifgebara/reactnative/master/lista/lista.json';
+
+    componentWillMount() {
         console.log('ListaCoisas.componentWillMount()');
         this.carregaLista();
     }
 
     carregaLista() {
-        setTimeout(() => {
-            console.log('Lista Carregada');
-            //this.state={COMPLETAMENTE ERRADO};
-            this.setState({
-                lista: [
-                    { id:1,nome: 'Arroz' },
-                    { id:2,nome: 'Feijão' }]
-            });
-        }
-            , 2000);
+        fetch(this.urlDaApi)
+            .then(resposta => {
+                resposta.json().then(data => {
+                    console.log(data);
+                    this.setState({ lista: data });
+                })
+
+                    .catch(erro => {
+                        this.setState(
+                            { lista: [{ id: 0, produto: 'Deu erro' }] }
+                        );
+                    })
+            })
+            ;
     }
 
-   renderCoisas(){
-       return this.state.lista.map(
-           coisa=><Text key={coisa.id}>{coisa.nome}</Text>
-       );
-   }
+    remove(coisa){
+        console.log('Removendo',coisa);
+    }
+
+    renderCoisas() {
+        const { imagemStyle, containerStyle } = estilos;
+        return this.state.lista.map(
+            coisa =>
+                <View style={containerStyle} key={coisa.id}>
+                    <Text>{coisa.produto}</Text>
+                    <Image style={imagemStyle} source={{ uri: coisa.imagem }} />
+                    <Button onPress={()=>{this.remove(coisa);}}>Remover</Button>
+                </View>
+
+        );
+    }
 
 
     render() {
         console.log('Lista.Render');
         return (
-            <View>
+            <ScrollView>
                 {this.renderCoisas()}
-            </View>
+            </ScrollView>
         );
     }
 }
+
+const estilos = {
+    imagemStyle: {
+        height: 300,
+        width: 300
+
+    },
+    containerStyle: {
+        alignItems: 'center',
+        flexDirection: 'column'
+    }
+};
+
 
 export default ListaCoisas;
