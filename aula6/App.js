147d4a2b609c070d550d9e91a6aba2bb8dc37069
Munif Gebara Junior <munifgebara@gmail.com>
2018-01-31 21:15:28 -0200

    logando

--- aula6/App.js
+++ aula6/App.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import { initializeApp} from 'firebase';
 import {
   Platform,
   StyleSheet,
@@ -15,6 +16,13 @@ import Login from './componentes/login';
 
 
 export default class App extends Component {
+
+  componentWillMount(){
+    initializeApp(config);
+
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
@@ -27,6 +35,16 @@ export default class App extends Component {
     );
   }
 }
+
+const config = {
+  apiKey: "AIzaSyDYnOYzNwnnn5NtCb6OdSwcCY1OvOnadE0",
+  authDomain: "aula-2018.firebaseapp.com",
+  databaseURL: "https://aula-2018.firebaseio.com",
+  projectId: "aula-2018",
+  storageBucket: "aula-2018.appspot.com",
+  messagingSenderId: "116877868019"
+};
+
 
 const styles = StyleSheet.create({
   container: {
--- aula6/componentes/login.js
+++ aula6/componentes/login.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, ScrollView } from 'react-native';
 import Button from './button';
+import { auth } from 'firebase';
 
 export default class Login extends Component {
 
-    state = {login:'',senha:''};
+    state = { login: '', senha: '', mensagem: 'inicio' };
 
     componentWillMount() {
     }
@@ -12,24 +13,33 @@ export default class Login extends Component {
     componentWillUnmount() {
     }
 
-    logar(){
-        console.log('State',this.state);
+    logar() {
+        console.log('State', this.state);
+        /*   auth().createUserWithEmailAndPassword('munif@munif.com.br','1234567')
+   .then(a=>{console.log('OK',a)})
+   .catch(b=>{console.log('Erro',b)}); 
+   */
+
+        auth().signInWithEmailAndPassword(this.state.login, this.state.senha)
+            .then(a => { this.setState({ mensagem: 'logado' }) })
+            .catch(a => { this.setState({ mensagem: 'erro' }) });
+
     }
 
 
     render() {
-        const {containerStyle}=estilos;
+        const { containerStyle } = estilos;
         return (
             <ScrollView>
                 <View style={containerStyle} >
-                <Text>Login</Text>
+                    <Text>Login</Text>
                     <TextInput
                         style={{ height: 20, width: 100 }}
                         value={this.state.login}
                         onChangeText={text => {
                             this.setState({ login: text });
                         }} />
-                        <Text>Senha</Text>
+                    <Text>Senha</Text>
                     <TextInput
                         style={{ height: 20, width: 100 }}
                         value={this.state.senha}
@@ -37,7 +47,8 @@ export default class Login extends Component {
                             this.setState({ senha: text });
                         }} />
                     <Button onPress={() => this.logar()}>Logar</Button>
-               </View>
+                    <Text>Mensage:{this.state.mensagem}</Text>
+                </View>
             </ScrollView>
         );
     }
